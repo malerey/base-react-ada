7bf77d4e302ded4b225e3d4a9c1ff5f527ad1b69
Malena Rey <malerey@male.localdomain>
2019-07-04 21:56:43 -0300

    Cambie el nombre de Ada

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return(
     <div className="App">
       <Persona
-      nombre="Ada"
+      nombre="Hedy Lamarr"
       bio="Creadora de la primera computadora"
       />
       <Persona
