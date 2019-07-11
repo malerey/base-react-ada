5354691494e07d4ee2be54036809d6f67d8b9fe1
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2019-07-11 15:52:51 -0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
-import Persona from './persona'
-
-
 
 function App() {
   return(
     <div className="App">
-      <Persona
-      nombre="Hedy Lamarr"
-      bio="Creadora de la primera computadora"
-      />
-      <Persona
-      nombre="Grace Hopper"
-      bio="Creadora del primer compilador"
-      />
+
     </div>
   );
 }
